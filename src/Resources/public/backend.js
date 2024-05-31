e4bf16c2e7885bc264880ffae266b685e93ab610
Jonathan Hermann <github@dievirtuellecouch.net>
2024-05-31 09:53:25 +0200

    Update backend script to init after document loaded

--- src/Resources/public/backend.js
+++ src/Resources/public/backend.js
@@ -1,8 +1,17 @@
 (function() {
-    const checkbox = document.querySelector('input[type="checkbox"][name="youtubeOptions"][value="youtube_nocookie"]');
-    if (checkbox === null) {
-        return;
+    const init = function() {
+        const checkbox = document.querySelector('input[type="checkbox"][name="youtubeOptions[]"][value="youtube_nocookie"]');
+        if (checkbox === null) {
+            return;
+        }
+    
+        checkbox.disabled = true;
     }
 
-    checkbox.disabled = true;
+    if (document.readyState === 'interactive' || document.readyState === 'complete' ) {
+        init();
+    }
+    else {
+        document.addEventListener('DOMContentLoaded', init);
+    }
 })();
